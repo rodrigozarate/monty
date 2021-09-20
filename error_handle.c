18f2256d064b84defe80be421cf6467cc2a42f0c
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-20 15:13:33 -0500

    [CREATE]

---
+++ error_handle.c
@@ -0,0 +1,58 @@
+/*
+* error_handle.c - Mesage errors according code
+* Author: Cristian Oliveros and Rodrigo Zarate A.
+* Date: September 20, 2021
+*/
+
+/*
+* If the user does not give any file or more than one argument to your program,
+* print the error message USAGE: monty file, followed by a new line, 
+* and exit with the status EXIT_FAILURE
+*
+* If, for any reason, it’s not possible to open the file,
+* print the error message Error: Can't open file <file>,
+* followed by a new line, and exit with the status EXIT_FAILURE
+* where <file> is the name of the file
+*
+* If the file contains an invalid instruction, print the error message 
+* L<line_number>: unknown instruction <opcode>, followed by a new line,
+* and exit with the status EXIT_FAILURE
+* where is the line number where the instruction appears.
+* Line numbers always start at 1
+*
+* If you can’t malloc anymore, print the error message 
+* Error: malloc failed, followed by a new line,
+* and exit with status EXIT_FAILURE.
+*/
+
+#include "header_monty.h"
+
+/**
+* error_handle - Shows erros acordingly to code
+* @error_code: code to select error to show
+* Return: Error
+*
+*/
+
+void error_handle(int error_code, ...)
+{
+	va_list error_list;
+	char *text_line;
+	int line_number;
+
+	va_start(error_list, error_code);
+	switch (error_code)
+	{
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		default;
+			break;
+	}
+	/* free list nodes */
+	/* exit */
+	exit(EXIT_FAILURE);
+}
---
+++ monty.c
@@ -0,0 +1,25 @@
+/*
+* monty.c - interpret monty opcodes
+* Autor: Cristian Oliveros and Rodrigo Zárate A.
+* Date: September 20, 2021
+*/
+
+#include "header_monty.h"
+
+/**
+* main - Entry Point
+* @argc: counter arguments
+* @argv: Array of arguments
+* Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	/* too much or too many arguments */
+	if (argc < 2 || argc > 2)
+		/* error hamndling */
+
+	/* Reach this point read the file */
+	open(argv[1]);
+	return (0);
+}
