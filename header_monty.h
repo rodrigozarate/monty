90f864c6321b5c7226f4b1ecd532c3fe2677d204
cristian oliveros <3308@holbertonschool.com>
2021-09-21 17:36:41 -0500

    [UPDATE] library

--- header_monty.h
+++ header_monty.h
@@ -2,6 +2,9 @@
 #define MONTY_H
 
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -36,6 +39,25 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+
 typedef void (*point_f)(stack_t **, unsigned int);
+stack_t *new_node(int n);
+void error_handle(int error_code, ...);
+void error_handle1(int error_code, ...);
+void open_file(char *file_name);
+void parse_file(FILE *monty_file);
+int parse_line(char *p_line, int line_number, int mode);
+void select_function(char *opcode, char *data, int line_number, int mode);
+void call_function(point_f f, char opcode, char *data, int line_number, int mode);
+void push(stack_t **stack, unsigned int line_number, int number);
+void pall(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void div(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
