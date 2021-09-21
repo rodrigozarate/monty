35e7536199affea9e515c241d69b80a7fb58c989
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-21 16:57:40 -0500

    [UPDATE] Add new function to error handle

--- error_handle.c
+++ error_handle.c
@@ -4,18 +4,6 @@
 * Date: September 20, 2021
 */
 
-/*
-* If the user does not give any file or more than one argument to your program,
-* print the error message USAGE: monty file, followed by a new line,
-*  and exit with the status EXIT_FAILURE
-*
-* If, for any reason, it’s not possible to open the file,
-* print the error message Error: Can't open file <file>,
-* followed by a new line, and exit with the status EXIT_FAILURE
-* where <file> is the name of the file
-*
-*/
-
 #include "header_monty.h"
 
 /**
@@ -35,23 +23,23 @@ void error_handle(int error_code, ...)
 	switch (error_code)
 	{
 	case 1:
-		fprintf(stderr, "L%d: usage: push integer\n",
+		printf(stderr, "L%d: usage: push integer\n",
 			line_number);
 		break;
 	case 2:
-		fprintf(stderr, "L%d: can't pint, stack empty\n",
+		printf(stderr, "L%d: can't pint, stack empty\n",
 			line_number);
 		break;
 	case 3:
-		fprintf(stderr, "L%d: can't pop an empty stack\n",
+		printf(stderr, "L%d: can't pop an empty stack\n",
 			line_number);
 		break;
 	case 4:
-		fprintf(stderr, "L%d: can't swap, stack too short\n",
+		printf(stderr, "L%d: can't swap, stack too short\n",
 			line_number);
 		break;
 	case 5:
-		fprintf(stderr, "L%d: can't add, stack too short\n",
+		printf(stderr, "L%d: can't add, stack too short\n",
 			line_number);
 		break;
 	case 6:
@@ -60,13 +48,44 @@ void error_handle(int error_code, ...)
 	case 7:
 		printf("Error: Can't open file %s\n",
 		       va_arg(error_list, char *));
+		break;
 	case 8:
 		line_number = va_arg(error_list, int);
 		opcode = va_arg(error_list, char *);
 		printf("L%d: unknown instruction %s\n", line_number, opcode);
+		break;
 	default;
-	break;
-	/* free list nodes */
+		break;
+	}
+	/* To do free list nodes */
 	/* exit */
 	exit(EXIT_FAILURE);
 }
+
+/**
+* error_handle1 - Shows erros acordingly to code
+* @error_code: code to select error to show
+* Return: Error
+*
+*/
+
+void error_handle1(int error_code, ...)
+{
+        va_list error_list;
+        char *text_line;
+        int line_number;
+
+        va_start(error_list, error_code);
+        switch (error_code)
+        {
+	case 10:
+		printf("USAGE: monty file\n");
+		break;
+	default;
+		break;
+	}
+	exit(EXIT_FAILURE);
+}
+
+
+
