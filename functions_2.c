d5ea7c458d36f89b705e0b1c60171e4ab6f54e40
cristian oliveros <3308@holbertonschool.com>
2021-09-22 17:09:41 -0500

    [FIX] changed range in pchar function

--- functions_2.c
+++ functions_2.c
@@ -131,7 +131,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 
 	if ((*stack)->n >= 32 && (*stack)->n <= 126)
 		printf("%c\n", (*stack)->n);
-	else
+	else if ((*stack)->n < 32 || (*stack)->n > 126)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n",
 			line_number);
